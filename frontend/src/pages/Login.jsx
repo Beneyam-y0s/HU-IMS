1cae7561bbdb5db306d4169628de849ef1b6cf8e
Beneyam Yohannes <yohannesgebretensae5@gmail.com>
2025-12-09 17:32:07 +0300

    Editing the login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -42,46 +42,47 @@ const Login = () => {
     };
 
     return (
-        <div className="flex flex-col items-center h-screen justify-center 
-            bg-gradient-to-b from-green-600 from-50% to-gray-100 to-50% space-y-6">
-            
-            <h2 className="text-3xl text-white font-bold">HU Inventory Management System</h2>
+        <div className="flex justify-center items-center w-full h-screen gap-4 ">
+            <div className="bg-green-600 h-screen w-[40%] flex justify-center items-center ">
+                <h2 className="text-3xl text-white font-bold ">HU Inventory Management System</h2>
+            </div>
+            <div className="h-screen w-[60%] flex justify-center items-center">
+                <div className="border shadow-lg p-6 w-80 bg-white ">
+                    <h2 className="text-2xl font-bold mb-4">Login</h2>
 
-            <div className="border shadow-lg p-6 w-80 bg-white">
-                <h2 className="text-2xl font-bold mb-4">Login</h2>
+                    {error && <div className="bg-red-200 text-red-700 p-2 mb-4 rounded">{error}</div>}
 
-                {error && <div className="bg-red-200 text-red-700 p-2 mb-4 rounded">{error}</div>}
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-4">
+                            <label className="block text-gray-700">Email</label>
+                            <input
+                                className="w-full px-3 py-2 border"
+                                type="text"
+                                onChange={(e) => setEmail(e.target.value)}
+                                placeholder="Enter your email"
+                                required
+                            />
+                        </div>
 
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-4">
-                        <label className="block text-gray-700">Email</label>
-                        <input
-                            className="w-full px-3 py-2 border"
-                            type="text"
-                            onChange={(e) => setEmail(e.target.value)}
-                            placeholder="Enter your email"
-                            required
-                        />
-                    </div>
+                        <div className="mb-4">
+                            <label className="block text-gray-700">Password</label>
+                            <input
+                                className="w-full px-3 py-2 border"
+                                type="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                                placeholder="Enter your password"
+                                required
+                            />
+                        </div>
 
-                    <div className="mb-4">
-                        <label className="block text-gray-700">Password</label>
-                        <input
-                            className="w-full px-3 py-2 border"
-                            type="password"
-                            onChange={(e) => setPassword(e.target.value)}
-                            placeholder="Enter your password"
-                            required
-                        />
-                    </div>
-
-                    <button
-                        type="submit"
-                        className="w-full bg-green-600 text-white py-2"
-                    >
-                        {loading ? "Loading..." : "Login"}
-                    </button>
-                </form>
+                        <button
+                            type="submit"
+                            className="w-full bg-green-600 text-white py-2"
+                        >
+                            {loading ? "Loading..." : "Login"}
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
     );
